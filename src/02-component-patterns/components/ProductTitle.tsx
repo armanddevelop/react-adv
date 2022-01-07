b4748b97e45ec818e935f982ef8c02632e586661
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-01-07 13:52:38 -0600

    clean the main branch to start other projects

--- src/02-component-patterns/components/ProductTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ProductContext } from "./ProductCard";
-import styles from "../styles/styles.module.css";
-
-export const ProductTitle = ()=>{
-  const {product} = useContext(ProductContext);
-  return(<span className={styles.productDescription}>{product.title}</span>)
-}
-export default ProductTitle;
